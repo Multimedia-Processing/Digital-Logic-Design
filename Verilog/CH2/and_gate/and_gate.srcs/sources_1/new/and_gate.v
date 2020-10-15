7b39a11e88848f044c782bc9253a3d60f58cb7c3
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-10-15 17:28:59 +0800

    如果採用，此提交將新增Verilog硬體描述語言實務第二單元完成
    修改項目:
    
    模組:
    
    議題

---
+++ Verilog/CH2/and_gate/and_gate.srcs/sources_1/new/and_gate.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/10/15 16:28:54
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+
+    );
+endmodule
