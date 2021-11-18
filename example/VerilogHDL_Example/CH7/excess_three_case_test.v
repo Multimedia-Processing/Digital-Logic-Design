dec35441c62d051b0ab8135d6ba46ebe385723e6
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-11-18 11:28:14 +0800

    此提交將加入匯入設計的晶片檔案，讓 vvp 可以讀取運作
    修改項目:
    
    模組:
    
    議題 4

--- example/VerilogHDL_Example/CH7/excess_three_case_test.v
+++ example/VerilogHDL_Example/CH7/excess_three_case_test.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "excess_three_case.v"
 
 module excess_three_case_test ();
 
@@ -12,6 +13,8 @@ initial begin
   for (conut = 0; conut < 8; conut = conut + 1)
     begin
       i = conut[2:0];
+      $monitor("%4dns monitor: conut=%d i=%b o=%b", $stime, conut, i, o);
+
       #10;
     end
 end
