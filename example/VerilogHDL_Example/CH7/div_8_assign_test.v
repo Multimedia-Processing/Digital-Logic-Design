56fa9e1749d1f4dd960fc8508b908f2ed955c4d0
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-12-17 17:17:59 +0800

    此提交將更新測試檔案，分隔兩個正增長與負增長
    修改項目:
    
    模組:
    
    議題4

--- example/VerilogHDL_Example/CH7/div_8_assign_test.v
+++ example/VerilogHDL_Example/CH7/div_8_assign_test.v
@@ -2,32 +2,30 @@
 `include "sub_4_assign.v"
 
 module sub_4_assign_test ();
-reg signed [3:0] a, b, c, d;
-wire signed [4:0] s1, s2;
+reg signed [7:0] a, b, c, d;
+wire signed [8:0] s1, s2;
 integer i;
 
 sub_4_assign UUT (.a(a), .b(b), .c(c), .d(d), .s1(s1), .s2(s2));
 
 initial begin
-  for (i = 0; i < 8; i = i + 1)
+  for (i = -64; i < 64; i = i + 1)
     begin
-      a = i * (-1 ** i);
-      b = (i - 1) * (-1 ** (i - 1));
+      a = i[7:0];
       #10;
     end
 end
 
 initial begin
-  for (i = 0; i < 8; i = i + 1)
+  for (i = 64; i < -64; i = i - 1)
     begin
-      c = i * (-1 ** i);
-      d = (i - 1) * (-1 ** (i - 1));
+      b = i;
       #10;
     end
 end
 
 initial begin
-  #80;
+  #1280;
   $finish;
 end
 
