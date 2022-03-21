7f4f480943ba1139101572c8affd747a08c53ca9
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-03-22 00:05:11 +0800

    此提交將完成5乘3乘法器
    修改項目:
    
    模組:
    
    議題

--- example/VerilogHDL_Example/CH7/mul_5x3_assign.v
+++ example/VerilogHDL_Example/CH7/mul_5x3_assign.v
@@ -1,11 +1,11 @@
-module mul_5x4_assign (a, b, c, d, s1, s2);
+module mul_5x3_assign (a, b, c, d, s1, s2);
 input [4:0] a;
 input [2:0] b;
 input signed [4:0] c;
 input signed [2:0] d;
-output signed [5:0] s1, s2;
+output signed [7:0] s1, s2;
 
-assign s1 = (a * b);
-assign s2 = ($signed(a) * $signed(b));
+assign s1 = ($signed(a) * $signed(b));
+assign s2 = (c * d);
 
-endmodule // mul_5x4_assign
+endmodule // mul_5x3_assign
--- example/VerilogHDL_Example/CH7/mul_5x3_assign_test.v
+++ example/VerilogHDL_Example/CH7/mul_5x3_assign_test.v
@@ -4,7 +4,7 @@
 module mul_5x3_assign_test ();
 reg signed [4:0] a, c;
 reg signed [2:0] b, d;
-wire signed [5:0] s1, s2;
+wire signed [7:0] s1, s2;
 integer number1, number2, number3, number4;
 
 mul_5x3_assign UUT (a, b, c, d, s1, s2);
@@ -12,11 +12,11 @@ mul_5x3_assign UUT (a, b, c, d, s1, s2);
 initial begin
   for (number1 = -8; number1 < 8; number1 = number1 + 1)
     begin
-      for (number2 = -8; number2 < 8; number2 = number2 + 1)
+      for (number2 = -4; number2 < 4; number2 = number2 + 1)
         begin
           for (number3 = -8; number3 < 8; number3 = number3 + 1)
             begin
-              for (number4 = -8; number4 < 8; number4 = number4 + 1)
+              for (number4 = -4; number4 < 4; number4 = number4 + 1)
                 begin
 
                   a = number1;
