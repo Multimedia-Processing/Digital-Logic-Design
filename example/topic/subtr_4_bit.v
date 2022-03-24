2c7e601bc265e7976258cb3834cf38b982360a28
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-03-25 00:02:26 +0800

    此提交將更新變數名稱為借位
    修改項目:
    
    模組:
    
    議題

--- example/topic/subtr_4_bit.v
+++ example/topic/subtr_4_bit.v
@@ -1,16 +1,16 @@
 `include "full_subtr.v"
 
-module subtr_4_bit (a, b, ci, sub, co);
+module subtr_4_bit (a, b, bi, sub, bo);
 
 input [3:0] a, b;
-input ci;
+input bi;
 output [3:0] sub;
-output co;
+output bo;
 wire borrow1, borrow2, borrow3;
 
-full_subtr add1(a[0], b[0], ci, sub[0], borrow1);
-full_subtr add2(a[1], b[1], borrow1, sub[1], borrow2);
-full_subtr add3(a[2], b[2], borrow2, sub[2], borrow3);
-full_subtr add4(a[3], b[3], borrow3, sub[3], co);
+full_subtr sub1(a[0], b[0], bi, sub[0], borrow1);
+full_subtr sub2(a[1], b[1], borrow1, sub[1], borrow2);
+full_subtr sub3(a[2], b[2], borrow2, sub[2], borrow3);
+full_subtr sub4(a[3], b[3], borrow3, sub[3], bo);
 
 endmodule // subtr_4_bit
