b08645d31080aac88a3ed93439588bdf94b6534f
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-03-24 21:34:15 +0800

    此提交將加上monitor可以做資訊的顯示，並減少測試的次數
    修改項目:
    
    模組:
    
    議題

--- example/topic/half_adder_test.v
+++ example/topic/half_adder_test.v
@@ -1,3 +1,5 @@
+`include "half_adder.v"
+
 module half_adder_test ();
   reg a, b;
   wire sum, carry;
@@ -6,12 +8,13 @@ module half_adder_test ();
   half_adder UUT (a, b, sum, carry);
 
   initial begin
-    for (number1 = 0; number1 < 4; number1 = number1 + 1)
+    for (number1 = 0; number1 < 2; number1 = number1 + 1)
       begin
-        for (number2 = 0; number2 < 4; number2 = number2 + 1)
+        for (number2 = 0; number2 < 2; number2 = number2 + 1)
           begin
             a = number1;
             b = number2;
+            $monitor("| %b | %b | %b | %b |", a, b, sum, carry);
             #10;
           end
       end
