083acf50347a4f9274f81c4493fe4e37a58b0ebe
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-03-25 00:02:40 +0800

    此提交將完成加減法器
    修改項目:
    
    模組:
    
    議題

--- example/topic/adder_subtr_4_bit.v
+++ example/topic/adder_subtr_4_bit.v
@@ -1,18 +1,18 @@
-`include "full_adder_4_bit.v"
+`include "adder_4_bit.v"
 
-module adder_subtr_4_bit (a, b, ci, m, s, co);
+module adder_subtr_4_bit (a, b, k, sum, co);
 
 input [3:0] a, b;
-input ci, m;
-output [3:0] s;
+input k;
+output [3:0] sum;
 output co;
 wire [3:0] add_sub;
 
 
-xor xor0(add_sub[0], , m);
-xor xor1(add_sub[1], , m);
-xor xor2(add_sub[2], , m);
-xor xor3(add_sub[3], m);
-full_adder_4_bit();
+xor xor0(add_sub[0], b[0], k);
+xor xor1(add_sub[1], b[1], k);
+xor xor2(add_sub[2], b[2], k);
+xor xor3(add_sub[3], b[3], k);
+adder_4_bit adder(a, add_sub, k, sum, co);
 
 endmodule // adder_subtr_4_bit
---
+++ example/topic/adder_subtr_4_bit_test.v
@@ -0,0 +1,30 @@
+`include "adder_subtr_4_bit.v"
+
+module adder_subtr_4_bit_test ();
+  reg [3:0] a, b;
+  reg k;
+  wire [3:0] sum;
+  wire co;
+  integer number1, number2, number3;
+
+  adder_subtr_4_bit UUT (a, b, k, sum, co);
+
+  initial begin
+    for (number1 = 0; number1 < 16; number1 = number1 + 1)
+      begin
+        for (number2 = 0; number2 < 16; number2 = number2 + 1)
+          begin
+            for (number3 = 0; number3 < 2; number3 = number3 + 1)
+              begin
+                a = number1;
+                b = number2;
+                k = number3;
+                $monitor("| %b | %b | %b | %b | %b |", a, b, k, sum, co);
+                #10;
+              end
+          end
+      end
+    $finish;
+  end
+
+endmodule // adder_subtr_4_bit_test
