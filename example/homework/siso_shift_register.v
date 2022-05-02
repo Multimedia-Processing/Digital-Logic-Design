1a323c7967c2bc6e211b7a2580b46cf943b36674
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-02 22:32:23 +0800

    此提交將新增串列輸入串列輸出
    修改項目:
    
    模組:
    
    議題

---
+++ example/homework/siso_shift_register.v
@@ -0,0 +1,19 @@
+module siso_shift_register (clock, clear, in, out);
+input clock, clear, in;
+output out;
+reg [3:0] register;
+
+always @ (posedge clock) begin
+  if (clear) begin
+    register = 4'd0;
+  end else begin
+    register[3] = register[2];
+    register[2] = register[1];
+    register[1] = register[0];
+    register[0] = in;
+  end
+assign out = register[3];
+
+end
+
+endmodule // siso_shift_register
