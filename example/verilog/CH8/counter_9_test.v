74c91eb93149f5e2671662740275d606a00eb480
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-04 16:38:56 +0800

    此提交將改變測試檔內電路設計方式
    修改項目:
    
    模組:
    
    議題

--- example/verilog/CH8/counter_9_test.v
+++ example/verilog/CH8/counter_9_test.v
@@ -1,34 +1,36 @@
 `include "counter_9.v"
 
 module counter_9_test ();
-  reg clock = 1'b0;
-  reg reset = 1'b0;
+  reg clock, reset;
+  integer number;
   wire [3:0] q;
 
-  parameter PERIOD = 200;
+  parameter PERIOD = 20;
   parameter real DUTY_CYCLE = 0.5;
   parameter OFFSET = 0;
+
+  counter_9 UUT (clock, reset, q);
+
+  initial begin
+    reset = 1'b0;
+    #5;
+    $display("| clock | reset |  q   |");
+    for (number = 0; number < 100; number = number + 1) begin
+      $monitor("|   %b   |   %b   | %b |", clock, reset, q);
+      #10;
+    end
+    $finish;
+  end
 
   initial begin
     #OFFSET;
     forever
       begin
         clock = 1'b0;
-        #(PERIOD - (PERIOD * DUTY_CYCLE)) clock = 1'b1;
+        #(PERIOD - (PERIOD * DUTY_CYCLE));
+        clock = 1'b1;
         #(PERIOD * DUTY_CYCLE);
       end
   end
 
-  counter_9 UUT (clock, reset, q);
-
-
-  initial begin
-    #2200;
-    $finish;
-  end
-
-  initial begin
-    #23;
-    reset = 1;
-  end
 endmodule // counter_9_test
