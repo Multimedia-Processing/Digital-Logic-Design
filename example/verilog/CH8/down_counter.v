679d2aa7d924af1577e66a67e7ef209bbcc8d082
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-04 17:51:18 +0800

    此提交將更新位元寬度不足問題
    修改項目:
    
    模組:
    
    議題

--- example/verilog/CH8/down_counter.v
+++ example/verilog/CH8/down_counter.v
@@ -1,8 +1,8 @@
 module down_counter (clock, reset, q);
 
 input clock, reset;
-output [3:0] q;
-reg [3:0] q;
+output [5:0] q;
+reg [5:0] q;
 
 always @ (posedge clock) begin
   if (reset | (q == 51)) begin
