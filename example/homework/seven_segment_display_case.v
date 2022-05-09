59bf0fe1386663fab87ccb088ea68c3f8e6c37a2
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-09 18:53:03 +0800

    此提交將修正錯誤的case語法，讓測試與結果正確
    修改項目:
    
    模組:
    
    議題

--- example/homework/seven_segment_display_case.v
+++ example/homework/seven_segment_display_case.v
@@ -1,26 +1,26 @@
 module seven_segment_display_case (data, display);
 input [3:0] data;
-output [7:0] display;
-reg [7:0] display;
+output [6:0] display;
+reg [6:0] display;
 
 
 always @ (data) begin
   case (data)
-    data == 4'b0000: display = 7'b1111110;
-    data == 4'b0001: display = 7'b0110000;
-    data == 4'b0010: display = 7'b1101101;
-    data == 4'b0011: display = 7'b1111001;
-    data == 4'b0100: display = 7'b0110011;
-    data == 4'b0101: display = 7'b1011011;
-    data == 4'b0110: display = 7'b1011111;
-    data == 4'b0111: display = 7'b1110010;
-    data == 4'b1000: display = 7'b1111111;
-    data == 4'b1001: display = 7'b1111011;
-    data == 4'b1010: display = 7'b1110111;
-    data == 4'b1011: display = 7'b0011111;
-    data == 4'b1100: display = 7'b1001110;
-    data == 4'b1101: display = 7'b0111101;
-    data == 4'b1110: display = 7'b1001111;
+    4'b0000: display = 7'b1111110;
+    4'b0001: display = 7'b0110000;
+    4'b0010: display = 7'b1101101;
+    4'b0011: display = 7'b1111001;
+    4'b0100: display = 7'b0110011;
+    4'b0101: display = 7'b1011011;
+    4'b0110: display = 7'b1011111;
+    4'b0111: display = 7'b1110010;
+    4'b1000: display = 7'b1111111;
+    4'b1001: display = 7'b1111011;
+    4'b1010: display = 7'b1110111;
+    4'b1011: display = 7'b0011111;
+    4'b1100: display = 7'b1001110;
+    4'b1101: display = 7'b0111101;
+    4'b1110: display = 7'b1001111;
     default: display = 7'b1000111;
   endcase
 end
