0dbee41dd9ddcbc99bd7bd4265b050767249523f
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-12 17:28:26 +0800

    此提交將修正寬度
    修改項目:
    
    模組:
    
    議題

--- example/verilog/CH8/counter_add_3.v
+++ example/verilog/CH8/counter_add_3.v
@@ -1,8 +1,8 @@
 module counter_add_3 (clock, reset, q);
 
 input clock, reset;
-output [3:0] q;
-reg [3:0] q;
+output [5:0] q;
+reg [5:0] q;
 
 always @ (posedge clock) begin
   if (reset | (q == 51)) begin
