ae33b9af56e89d86a3b37f31142dcaf0870d6488
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-12 17:28:11 +0800

    此提交將修正10進位下數器
    修改項目:
    
    模組:
    
    議題

--- example/verilog/CH8/down_counter.v
+++ example/verilog/CH8/down_counter.v
@@ -1,14 +1,14 @@
 module down_counter (clock, reset, q);
 
 input clock, reset;
-output [5:0] q;
-reg [5:0] q;
+output [3:0] q;
+reg [3:0] q;
 
 always @ (posedge clock) begin
-  if (reset | (q == 51)) begin
-    q = 0;
+  if (reset | (q == 0)) begin
+    q = 9;
   end else begin
-    q = q + 3;
+    q -= 1;
   end
 end
 
