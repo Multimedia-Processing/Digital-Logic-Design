19b19ea162dea7cd6d585a2bdbe7c6046cd01570
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-13 12:57:06 +0800

    此提交將加入上數七段顯示器
    修改項目:
    
    模組:
    
    議題

--- example/topic/up_counter_seven_segment_display.v
+++ example/topic/up_counter_seven_segment_display.v
@@ -1,3 +1,12 @@
-module up_counter_seven_segment_display ();
+`include "counter.v"
+`include "seven_segment_display_assign.v"
+
+module up_counter_seven_segment_display (clock, reset, display);
+input clock, reset;
+output [6:0] display;
+wire [3:0] data;
+
+counter count(clock, reset, data);
+seven_segment_display_assign seven_seg(data, display);
 
 endmodule // up_counter_seven_segment_display
