82f18a88ae9b05163b8eb333ec5f4ce73ac7034d
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-13 12:57:20 +0800

    此提交將加入測試檔案
    修改項目:
    
    模組:
    
    議題

---
+++ example/topic/up_counter_seven_segment_display_test.v
@@ -0,0 +1,39 @@
+`include "up_counter_seven_segment_display.v"
+
+module up_counter_seven_segment_display_test ();
+  reg clock, reset;
+  integer number;
+  wire [6:0] display;
+
+  up_counter_seven_segment_display UUT (clock, reset, display);
+
+  initial begin
+    clock = 1'b1;
+    reset = 1'b1;
+
+    #10;
+    reset = 1'b0;
+
+    #5;
+    $display("| clock | reset | display |");
+    for (number = 0; number < 50; number = number + 1) begin
+      $monitor("|   %b   |   %b   | %b |", clock, reset, display);
+      #10;
+    end
+    $finish;
+  end
+
+  initial begin
+    #250;
+    reset = 1'b1;
+    #20;
+    reset = 1'b0;
+
+  end
+
+  always begin
+    #10;
+    clock = ~clock;
+  end
+
+endmodule // up_counter_seven_segment_display_test
