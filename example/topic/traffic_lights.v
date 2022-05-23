7ead0601b2905da7f83c8cecb787f960076f5ea7
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-23 21:04:38 +0800

    此提交將定義資料格式
    修改項目:
    
    模組:
    
    議題

--- example/topic/traffic_lights.v
+++ example/topic/traffic_lights.v
@@ -4,6 +4,15 @@
 
 module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
   row_display, column_display);
+input clock, reset;
+output [2:0] row_traffic_lights, column_traffic_lights;
+output [13:0] row_display, column_display;
+reg [13:0] row_display, column_display;
+reg [2:0] row_traffic_lights, column_traffic_lights;
+reg [5:0] coder;
+reg [19:0] decoder;
+wire [7:0] row_bcd, column_bcd;
+
 endmodule // traffic_lights
 
 module down_counter (clock, reset, q);
