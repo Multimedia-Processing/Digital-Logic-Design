34dd64125d50fd6be80745b77d668c3aa134edb5
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-23 20:58:16 +0800

    此提交將新增下數64次的下數器
    修改項目:
    
    模組:
    
    議題

--- example/topic/traffic_lights.v
+++ example/topic/traffic_lights.v
@@ -1,3 +1,19 @@
 module traffic_lights ();
 
 endmodule // traffic_lights
+
+module down_counter (clock, reset, q);
+
+  input clock, reset;
+  output [5:0] q;
+  reg [5:0] q;
+
+  always @ (posedge clock) begin
+    if (reset | (q == 0)) begin
+      q = 64;
+    end else begin
+      q -= 1;
+    end
+  end
+
+endmodule // down_counter
