f7618f7cc115f8ad7e7749a5ebbcf79b51e21c02
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-23 20:05:14 +0800

    此提交將新增32單位除頻器
    修改項目:
    
    模組:
    
    議題

---
+++ example/homework/frequency_divder_32_test.v
@@ -0,0 +1,26 @@
+module frequency_divder_32_test (clock, reset, out);
+  reg clock;
+  reg reset;
+
+  frequency_divder_32 UUT ();
+
+  initial begin
+    clock = 1'b1;
+    reset = 1'b0;
+    #5;
+
+    reset = 1'b1;
+
+  end
+
+  always begin
+    #10;
+    clock = ~clock;
+  end
+
+  initial begin
+    #100000;
+    $finish;
+  end
+
+endmodule // frequency_divder_32_test
