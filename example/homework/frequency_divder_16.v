f2c08f0452f2294e473296dd070c69f577557f8a
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-25 00:41:21 +0800

    此提交將新增16除頻器
    修改項目:
    
    模組:
    
    議題

---
+++ example/homework/frequency_divder_16.v
@@ -0,0 +1,38 @@
+// 32除頻器
+
+module frequency_divder_16 (clock, reset, out);
+  input clock, reset;
+  output out;
+
+  reg out;
+  reg [3:0] diver;
+
+  parameter count = 16;
+
+  always @ (posedge clock) begin
+    if (~reset) begin
+      diver = 0;
+    end else begin
+      if (diver < count - 1) begin
+        diver += 1;
+      end else begin
+        diver = 0;
+      end
+    end
+
+  end
+
+  // 產生工作週期 0.03125 波形
+  always @ (posedge clock) begin
+    if (~reset) begin
+      out = 1'b0;
+    end else begin
+      if (diver == count - 1) begin
+        out = ~out;
+      end else begin
+        out = out;
+      end
+    end
+  end
+
+endmodule // frequency_divder_16
---
+++ example/homework/frequency_divder_16_test.v
@@ -0,0 +1,36 @@
+`include "frequency_divder_16.v"
+
+module frequency_divder_16_test ();
+  reg clock;
+  reg reset;
+  wire out;
+
+  integer number;
+
+  frequency_divder_16 UUT (clock, reset, out);
+
+  initial begin
+    clock = 1'b1;
+    reset = 1'b0;
+    #5;
+
+    reset = 1'b1;
+
+    $display("| clock | reset | out |");
+
+  end
+
+  always begin
+    #5;
+    $monitor("|   %b   |   %b   |  %b  |", clock, reset, out);
+
+    #5;
+    clock = ~clock;
+  end
+
+  initial begin
+    #1000;
+    $finish;
+  end
+
+endmodule // frequency_divder_16_test
