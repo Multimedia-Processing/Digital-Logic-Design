5aeabc19de6be704cc0bacf8a73cf08ffd7b0f48
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-24 15:19:08 +0800

    此提交將完成10BCD除頻器
    修改項目:
    
    模組:
    
    議題

---
+++ example/homework/frequency_divder_bcd.v
@@ -0,0 +1,26 @@
+module frequency_divder_bcd (clock, reset, out);
+  input clock, reset;
+  output out;
+
+  reg out;
+  reg [3:0] diver;
+
+  // 10 除頻器
+  always @ (posedge clock) begin
+    if (~reset || diver == 9) begin
+      diver = 0;
+    end else begin
+      diver += 1;
+    end
+  end
+
+  // 形成對稱方波
+  always @ (diver) begin
+    if (diver < 5) begin
+      out = 0;
+    end else begin
+      out = 1;
+    end
+  end
+
+endmodule // frequency_divder_bcd
