9c46ba2c20dd142f23f00fee93fe3ac7e77e001e
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-05-25 00:48:41 +0800

    此提交將新增工作週期50的500除頻器
    修改項目:
    
    模組:
    
    議題

---
+++ example/homework/frequency_divder_500_duty_cycle_50.v
@@ -0,0 +1,26 @@
+module frequency_divder_500_duty_cycle_50 (clock, reset, out);
+  input clock, reset;
+  output out;
+
+  reg out;
+  reg [8:0] diver;
+
+  // 500 除頻器
+  always @ (posedge clock) begin
+    if (~reset || diver == 500) begin
+      diver = 0;
+    end else begin
+      diver += 1;
+    end
+  end
+
+  // 形成對稱方波
+  always @ (diver) begin
+    if (diver < 250) begin
+      out = 0;
+    end else begin
+      out = 1;
+    end
+  end
+
+endmodule // frequency_divder_500_duty_cycle_50
