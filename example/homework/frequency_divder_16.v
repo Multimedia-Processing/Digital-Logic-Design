ae40acb21c6956b0a9fc2ad48cb81c77faafaa85
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-06-13 00:07:42 +0800

    此提交將修正32除頻器
    修改項目:
    
    將前緣觸發改為準位觸發，且觸發條件為diver的數字更新
    
    模組:
    
    議題

--- example/homework/frequency_divder_16.v
+++ example/homework/frequency_divder_16.v
@@ -14,16 +14,15 @@ module frequency_divder_16 (clock, reset, out);
       diver = 0;
     end else begin
       if (diver < count - 1) begin
-        diver += 1;
+        diver = diver + 1;
       end else begin
         diver = 0;
       end
     end
-
   end
 
   // 產生工作週期 0.03125 波形
-  always @ (posedge clock) begin
+  always @ (diver) begin
     if (~reset) begin
       out = 1'b0;
     end else begin
