7668389e798101fa4a768fbc089cd000d018f52e
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-07-13 17:39:10 +0800

    此提交將更新語法讓資料正常的做判斷式
    修改項目:
    
    模組:
    
    議題12

--- example/topic/decoder_6x20.v
+++ example/topic/decoder_6x20.v
@@ -3,7 +3,7 @@ module decoder_6x20 (clock, reset, coder, decoder);
   input [5:0] coder;
   output [19:0] decoder;
   reg [2:0] row_traffic_lights, column_traffic_lights;
-  reg [7:0] row_binary, column_binary;
+  reg [6:0] row_binary, column_binary;
 
   always @ (posedge clock) begin
     if (reset) begin
@@ -12,30 +12,35 @@ module decoder_6x20 (clock, reset, coder, decoder);
       row_traffic_lights = 0;
       column_traffic_lights = 0;
 
-    end else if (0 < coder <= 28) begin
+    end else if (0 <= coder && coder < 28) begin
       row_binary += 1;
       column_binary = 0;
       row_traffic_lights = 3'b001;
       column_traffic_lights = 3'b100;
 
-    end else if (28 < coder <= 32) begin
+    end else if (28 <= coder && coder < 32) begin
       row_binary = 0;
       column_binary = 0;
       row_traffic_lights = 3'b010;
       column_traffic_lights = 3'b100;
 
-    end else if (32 < coder <= 60) begin
+    end else if (32 <= coder && coder < 60) begin
       row_binary = 0;
       column_binary += 1;
       row_traffic_lights = 3'b100;
       column_traffic_lights = 3'b001;
 
-    end else if (60 < coder <= 64) begin
+    end else if (60 <= coder && coder < 64) begin
       row_binary = 0;
       column_binary = 0;
       row_traffic_lights = 3'b100;
       column_traffic_lights = 3'b010;
 
+    end else begin
+        row_binary = 7'b1111111;
+        column_binary = 7'b1111111;
+        row_traffic_lights = 3'b111;
+        column_traffic_lights = 3'b111;
     end
   end
 
