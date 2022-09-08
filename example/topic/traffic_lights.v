483864cb45a9336260df195912bd92dd90a4a213
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-08 22:59:25 +0800

    此提交將更新輸出結果
    修改項目:
    
    模組:
    
    議題12

--- example/topic/traffic_lights.v
+++ example/topic/traffic_lights.v
@@ -8,21 +8,22 @@ module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
 
   input clock, reset;
   output [2:0] row_traffic_lights, column_traffic_lights;
-  output [13:0] row_display, column_display;
-  wire [13:0] row_display, column_display;
-  wire [2:0] row_traffic_lights, column_traffic_lights;
+  output  [13:0] row_display, column_display;
+  reg [2:0] row_traffic_lights, column_traffic_lights;
+  reg [13:0] row_display, column_display;
+
   wire [5:0] coder;
   wire [19:0] decoder;
   wire [7:0] row_bcd, column_bcd;
 
   counter_down_64 count (clock, reset, coder);
   decoder_6x20 dcode (clock, reset, coder, decoder);
-  binary_to_bcd row_bcd_circuit(decoder[0:6], row_bcd);
-  binary_to_bcd column_bcd_circuit(decoder[7:13], column_bcd);
-  seven_segment_display_assign seven_seg_row_0(row_bcd[0:3], row_display[0:6]);
-  seven_segment_display_assign seven_seg_row_1(row_bcd[4:7], row_display[6:13]);
-  seven_segment_display_assign seven_seg_column_0(column_bcd[0:3], column_display[0:6]);
-  seven_segment_display_assign seven_seg_column_1(column_bcd[4:7], column_display[6:13]);
+  binary_to_bcd column_bcd_circuit(decoder[13:7], column_bcd);
+  binary_to_bcd row_bcd_circuit(decoder[6:0], row_bcd);
+  seven_segment_display_assign seven_seg_row_0(row_bcd[3:0], row_display[6:0]);
+  seven_segment_display_assign seven_seg_row_1(row_bcd[7:4], row_display[13:6]);
+  seven_segment_display_assign seven_seg_column_0(column_bcd[3:0], column_display[6:0]);
+  seven_segment_display_assign seven_seg_column_1(column_bcd[7:4], column_display[13:6]);
 
   always @ (posedge clock) begin
     if (28 < coder <= 32) begin
