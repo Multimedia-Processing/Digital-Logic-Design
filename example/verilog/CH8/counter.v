28e6cabd8081110badf3cc58db0253b54c7497b0
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-08 22:59:07 +0800

    此提交將更新結構內容，可能會失敗或出錯
    修改項目:
    
    模組:
    
    議題12

--- example/verilog/CH8/counter.v
+++ example/verilog/CH8/counter.v
@@ -4,12 +4,10 @@ input clock, reset;
 output [3:0] q;
 reg [3:0] q;
 
-always @ (posedge clock) begin
-  if (reset | (q == 9)) begin
+always @ (posedge clock)
+  if (reset | (q == 9))
     q = 0;
-  end else begin
+  else
     q += 1;
-  end
-end
 
 endmodule // counter
