665b1b9997b0a60cc1a9483cf62fc80af0ece38d
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-10-19 11:40:56 +0800

    此提交將完成紅綠燈設計，內容目前尚未驗證
    修改項目:
    
    修改為對應新的解碼器
    
    將reg的資料指定，改為使用wire連接
    
    刪除沒必要的程式，因為解碼器可以依照計數器給予的值做對應輸出，而不需要另外寫判斷式去偵測數值是否正確。
    
    模組:
    
    議題 #12

--- example/topic/traffic_lights.v
+++ example/topic/traffic_lights.v
@@ -9,15 +9,15 @@ module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
   input clock, reset;
   output [2:0] row_traffic_lights, column_traffic_lights;
   output  [13:0] row_display, column_display;
-  reg [2:0] row_traffic_lights, column_traffic_lights;
-  reg [13:0] row_display, column_display;
 
+  wire [2:0] row_traffic_lights, column_traffic_lights;
+  wire [13:0] row_display, column_display;
   wire [5:0] coder;
   wire [19:0] decoder;
   wire [7:0] row_bcd, column_bcd;
 
   counter_down_64 count (clock, reset, coder);
-  decoder_6x20 dcode (clock, reset, coder, decoder);
+  decoder_6x20 dcode (coder, decoder);
   binary_to_bcd column_bcd_circuit(decoder[13:7], column_bcd);
   binary_to_bcd row_bcd_circuit(decoder[6:0], row_bcd);
   seven_segment_display_assign seven_seg_row_0(row_bcd[3:0], row_display[6:0]);
@@ -25,24 +25,7 @@ module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
   seven_segment_display_assign seven_seg_column_0(column_bcd[3:0], column_display[6:0]);
   seven_segment_display_assign seven_seg_column_1(column_bcd[7:4], column_display[13:7]);
 
-  always @ (posedge clock) begin
-    if (28 < coder <= 32) begin
-      row_display = 0;
-      {row_traffic_lights, column_traffic_lights} = decoder[8:19];
-
-    end else if (60 < coder <= 64) begin
-      row_display = 0;
-      {row_traffic_lights, column_traffic_lights} = decoder[8:19];
-
-    end else if (60 < coder <= 64) begin
-      row_display = 0;
-      {row_traffic_lights, column_traffic_lights} = decoder[8:19];
-
-    end else if (60 < coder <= 64) begin
-      row_display = 0;
-      {row_traffic_lights, column_traffic_lights} = decoder[8:19];
-
-    end
-  end
+  assign row_traffic_lights = decoder[19:17];
+  assign column_traffic_lights = decoder[16:15];
 
 endmodule // traffic_lights
