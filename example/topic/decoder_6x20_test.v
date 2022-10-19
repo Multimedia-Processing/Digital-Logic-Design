d1e1963691f9435abaa288696bda2abcfe4453fe
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-10-19 11:37:47 +0800

    此提交將修改測試檔案
    修改項目:
    
    刪減輸入，時脈與重置
    
    修改顯示資料方式，讓reset的時候也可以顯示
    
    模組:
    
    議題12

--- example/topic/decoder_6x20_test.v
+++ example/topic/decoder_6x20_test.v
@@ -1,22 +1,16 @@
 `include "decoder_6x20.v"
 
 module decoder_6x20_test ();
-  reg clock, reset;
   reg [5:0] coder;
   integer number;
   wire [19:0] decoder;
 
-  decoder_6x20 UUT (clock, reset, coder, decoder);
+  decoder_6x20 UUT (coder, decoder);
 
   initial begin
-    $display("| clock | reset | coder | decoder |");
-    clock = 1'b1;
-    reset = 1'b1;
+    $display("| coder | decoder |");
 
-    #10;
-    reset = 1'b0;
-
-    #5;
+    #15;
     for (number = 0; number < 65; number = number + 1) begin
       coder = number;
       #20;
@@ -24,19 +18,10 @@ module decoder_6x20_test ();
     $finish;
   end
 
-  initial begin
-    #250;
-    reset = 1'b1;
-    #20;
-    reset = 1'b0;
-
-  end
-
   always begin
     #5;
-    $monitor("|   %b   |   %b   | %d | %b |", clock, reset, coder, decoder);
+    $monitor("| %d | %b |", coder, decoder);
     #5;
-    clock = ~clock;
   end
 
 endmodule // decoder_6x20_test
