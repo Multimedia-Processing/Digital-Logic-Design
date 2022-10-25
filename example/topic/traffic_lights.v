61ea352a1c00e30db4cfeb44acc2af51f2cecaae
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-10-25 20:17:26 +0800

    此提交將使顯示器可以依照正常顯示七段顯示器並倒數，但時間未正確每秒，而是稍快
    修改項目:
    
    模組:
    
    議題

--- example/topic/traffic_lights.v
+++ example/topic/traffic_lights.v
@@ -12,9 +12,9 @@ module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
 	output [7:0] turn;
 
 	reg [7:0] turn;
-	reg [27:0] diver;
-	reg clock_1hz;
-	reg [3:0] turn_row_bcd, turn_column_bcd;
+	reg [27:0] diver, diver250;
+	reg clock_1hz, clock_250hz;
+	reg [3:0] turn_bcd;
 
 	wire [7:0] row_bcd, column_bcd;
 	wire [6:0] row_display, column_display;
@@ -38,7 +38,23 @@ module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
 		end
 	end
 
-	always @ (posedge clock) begin
+	always@(posedge clock) begin
+		if(reset || diver250 == 400000) begin
+			diver250 = 0;
+		end else begin
+			diver250 = diver250 + 1;
+		end
+	end
+
+	always @ (diver250) begin
+		if (diver250 < 200000) begin
+			clock_250hz = 1;
+		end else begin
+			clock_250hz = 0;
+		end
+	end
+
+	always @ (posedge clock_250hz) begin
 		if (turn == 0 || reset) begin
 			turn = 8'b00000001;
 		end else begin
@@ -46,12 +62,12 @@ module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
 		end
 	end
 
-	always @ (posedge clock) begin
+	always @ (turn) begin
 		case (turn)
-			8'b00000001: turn_row_bcd = column_bcd[3:0];
-			8'b00000100: turn_row_bcd = column_bcd[7:0];
-			8'b00010000: turn_column_bcd = row_bcd[3:0];
-			8'b01000000: turn_column_bcd = row_bcd[7:4];
+			8'b00000001: turn_bcd = column_bcd[3:0];
+			8'b00000100: turn_bcd = column_bcd[7:4];
+			8'b00010000: turn_bcd = row_bcd[3:0];
+			8'b01000000: turn_bcd = row_bcd[7:4];
 		endcase
 	end
 
@@ -60,10 +76,10 @@ module traffic_lights (clock, reset, row_traffic_lights, column_traffic_lights,
 	binary_to_bcd column_bcd_circuit(decoder[13:7], column_bcd);
 	binary_to_bcd row_bcd_circuit(decoder[6:0], row_bcd);
 
+	seven_segment_display_assign seven_seg_row(turn_bcd, row_display);
+	seven_segment_display_assign seven_seg_column(turn_bcd, column_display);
+
 	assign row_traffic_lights = decoder[19:17];
 	assign column_traffic_lights = decoder[16:14];
-
-	seven_segment_display_assign seven_seg_row_0(turn_row_bcd, row_display);
-	seven_segment_display_assign seven_seg_row_1(turn_column_bcd, column_display);
 
 endmodule // traffic_lights
