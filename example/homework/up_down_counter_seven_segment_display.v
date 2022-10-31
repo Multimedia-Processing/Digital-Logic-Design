4181879d7c73a99f850cf0ff8144d1b6b83e68ff
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-10-31 19:09:40 +0800

    此提交將修正錯誤內容
    修改項目:
    
    模組:
    
    議題44

--- example/homework/up_down_counter_seven_segment_display.v
+++ example/homework/up_down_counter_seven_segment_display.v
@@ -1,12 +1,13 @@
 `include "seven_segment_display_assign.v"
 
-module up_down_counter_seven_segment_display (clock, reset, display, turn, control);
-    input clock, reset;
+module up_down_counter_seven_segment_display (clock, reset, control, display, turn);
+    input clock, reset, control;
     output [6:0] display;
+    output [3:0] turn;
 
     reg [3:0] data;
     reg [27:0] diver;
-    reg clock_1hz, control;
+    reg clock_1hz;
     reg [3:0] turn = 4'b0001;
 
     always@(posedge clock) begin
@@ -25,18 +26,18 @@ module up_down_counter_seven_segment_display (clock, reset, display, turn, contr
         end
     end
 
-    always @ (posedge clock) begin
+    always @ (posedge clock_1hz) begin
         if (control == 1) begin
-            if (reset || q == 9) begin
-                q = 0;
+            if (reset || data == 9) begin
+                data = 0;
             end else begin
-                q += 1;
+                data = data + 1;
             end
         end else begin
             if (reset || data == 0) begin
                 data = 9;
             end else begin
-                data -= 1;
+                data = data - 1;
             end
         end
     end
