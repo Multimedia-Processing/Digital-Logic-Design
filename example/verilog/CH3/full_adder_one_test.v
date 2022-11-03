a2708d5841d3da4dd5c90b8dffe9eb1f87b5ab9d
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-11-03 23:14:13 +0800

    Relate # ! 提交將更新為可以用在iverilog與vivado內容
    修改項目:
    
    模組:

--- example/verilog/CH3/full_adder_one_test.v
+++ example/verilog/CH3/full_adder_one_test.v
@@ -1,30 +1,38 @@
-`timescale 1ns / 1ps
 `include "full_adder_one.v"
 
 module full_adder_one_test ();
 
-reg a = 1'b0;
-reg b = 1'b0;
-reg ci = 1'b0;
+reg a, b, ci;
 
 wire co, s;
 
 full_adder_one UUT (a, b, ci, co, s);
 
 initial begin
+
+  $display("| a | b | ci | co | s|");
+  #100; {a, b, ci} = 3'b000;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
   #100; {a, b, ci} = 3'b001;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
   #100; {a, b, ci} = 3'b010;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
   #100; {a, b, ci} = 3'b011;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
   #100; {a, b, ci} = 3'b100;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
   #100; {a, b, ci} = 3'b101;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
   #100; {a, b, ci} = 3'b110;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
   #100; {a, b, ci} = 3'b111;
+  $monitor("| %b | %b | %b | %b | %b |", a, b, ci, co, s);
 
 end
 
 initial begin
   #900;
-  $stop;
+  $finish;
 end
 
 endmodule // full_adder_one_test
