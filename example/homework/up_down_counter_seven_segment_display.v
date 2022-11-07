2db5f3aff4d01f2ab167e057fc656389690cf0b2
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-11-07 19:43:23 +0800

    Relate # ! 提交將取消除頻部份，加入除頻後結果等於clock，用於測試
    修改項目:
    
    模組:
    
    議題

--- example/homework/up_down_counter_seven_segment_display.v
+++ example/homework/up_down_counter_seven_segment_display.v
@@ -10,21 +10,24 @@ module up_down_counter_seven_segment_display (clock, reset, control, display, tu
     reg clock_1hz;
     reg [3:0] turn = 4'b0001;
 
-    always@(posedge clock) begin
-        if(reset || diver == 100000000) begin
-            diver = 0;
-        end else begin
-            diver = diver + 1;
-        end
-    end
-
-    always @ (diver) begin
-        if (diver < 50000000) begin
-            clock_1hz = 1;
-        end else begin
-            clock_1hz = 0;
-        end
-    end
+    // always@(posedge clock) begin
+    //     if(reset || diver == 100000000) begin
+    //         diver = 0;
+    //     end else begin
+    //         diver = diver + 1;
+    //     end
+    // end
+	//
+    // always @ (diver) begin
+    //     if (diver < 50000000) begin
+    //         clock_1hz = 1;
+    //     end else begin
+    //         clock_1hz = 0;
+    //     end
+    // end
+	always @ (clock) begin
+		clock_1hz = clock;
+	end
 
     always @ (posedge clock_1hz) begin
         if (control == 1) begin
