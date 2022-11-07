810e75ee62e3e81f726b7da1aabb0910c8a989b1
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-11-07 22:00:42 +0800

    Relate #47 !28 提交將轉為模擬電路可以測試
    修改項目:
    
    模組:
    
    議題

--- example/homework/perak_light.v
+++ example/homework/perak_light.v
@@ -13,43 +13,43 @@ module perak_light (clock, reset, display, turn);
     wire [3:0] data;
 
     // 此用於實際電路除頻器，實際燒錄時取消註解
-    always@(posedge clock) begin
-        if(reset || diver == 10000000) begin
-            diver = 0;
-        end else begin
-            diver = diver + 1;
-        end
-    end
-
-    always @ (diver) begin
-        if (diver < 5000000) begin
-            clock_10hz = 1;
-        end else begin
-            clock_10hz = 0;
-        end
-    end
-
-    always@(posedge clock) begin
-        if(reset || diver250 == 400000) begin
-            diver250 = 0;
-        end else begin
-            diver250 = diver250 + 1;
-        end
-    end
-
-    always @ (diver250) begin
-        if (diver250 < 200000) begin
-            clock_250hz = 1;
-        end else begin
-            clock_250hz = 0;
-        end
-    end
+    // always@(posedge clock) begin
+    //     if(reset || diver == 10000000) begin
+    //         diver = 0;
+    //     end else begin
+    //         diver = diver + 1;
+    //     end
+    // end
+    //
+    // always @ (diver) begin
+    //     if (diver < 5000000) begin
+    //         clock_10hz = 1;
+    //     end else begin
+    //         clock_10hz = 0;
+    //     end
+    // end
+    //
+    // always@(posedge clock) begin
+    //     if(reset || diver250 == 400000) begin
+    //         diver250 = 0;
+    //     end else begin
+    //         diver250 = diver250 + 1;
+    //     end
+    // end
+    //
+    // always @ (diver250) begin
+    //     if (diver250 < 200000) begin
+    //         clock_250hz = 1;
+    //     end else begin
+    //         clock_250hz = 0;
+    //     end
+    // end
 
     // 此用於模擬檢測使用，實際燒錄時將此註解
-    // always @ (clock) begin
-    //     clock_10hz = clock;
-    //     clock_250hz = clock;
-    // end
+    always @ (clock) begin
+        clock_10hz = clock;
+        clock_250hz = clock;
+    end
 
     always @ (data) begin
         case (data)
