91bcfcf8e300853990a3541d724624278cc5f6e8
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-11-07 19:42:41 +0800

    Relate #44 !20 提交將重新設計與製作測試內容
    修改項目:
    
    模組:
    
    議題

--- example/homework/up_down_counter_seven_segment_display_test.v
+++ example/homework/up_down_counter_seven_segment_display_test.v
@@ -1,39 +1,44 @@
 `include "up_down_counter_seven_segment_display.v"
 
 module up_down_counter_seven_segment_display_test ();
-  reg clock, reset;
-  integer number;
-  wire [6:0] display;
+	reg clock, reset, control;
+	integer number;
+	wire [6:0] display;
+	wire [3:0] turn;
 
-  up_down_counter_seven_segment_display UUT (clock, reset, display);
+	up_down_counter_seven_segment_display UUT (
+		clock, reset, control, display, turn);
 
-  initial begin
-    clock = 1'b1;
-    reset = 1'b1;
+	initial begin
+		$display("| clock | reset | control | display | turn |");
+		clock = 1'b1;
+		reset = 1'b1;
+		control = 1'b1;
 
-    #10;
-    reset = 1'b0;
+		#10;
+		reset = 1'b0;
 
-    #5;
-    $display("| clock | reset | display |");
-    for (number = 0; number < 50; number = number + 1) begin
-      $monitor("|   %b   |   %b   | %b |", clock, reset, display);
-      #10;
-    end
-    $finish;
-  end
+		#5;
+    	#100;
+	    reset = 1'b1;
+	    #10;
+		reset = 1'b0;
+		#10;
+		control = 1'b0;
+		#100;
+		control = 1'b1;
 
-  initial begin
-    #250;
-    reset = 1'b1;
-    #20;
-    reset = 1'b0;
+		$finish;
+	end
 
-  end
+	always begin
+		#5;
+		$monitor(
+			"|   %b   |   %b   | %b | %b | %b |",
+			clock, reset, control, display, turn);
 
-  always begin
-    #10;
-    clock = ~clock;
-  end
+		#5;
+		clock = ~clock;
+	end
 
 endmodule // up_down_counter_seven_segment_display_test
