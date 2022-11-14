edcf619ab5a4ee831ce2b5bc51e586376cde1636
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2022-11-14 20:49:35 +0800

    Relate #47 !28 提交將完成霹靂燈
    修改項目:
    
    模組:
    
    議題

--- example/homework/perak_light.v
+++ example/homework/perak_light.v
@@ -5,13 +5,12 @@ module perak_light (clock, reset, display, turn);
     output [6:0] display;
     output [3:0] turn;
 
-    reg [27:0] diver, diver250;
-    reg clock_10hz, clock_250hz;
+    reg [27:0] diver;
+    reg clock_1hz
+    ;
     reg [3:0] turn = 4'b0001;
     reg [6:0] display;
     reg [3:0] data;
-
-    wire [3:0] data;
 
     // 此用於實際電路除頻器，實際燒錄時取消註解
     always@(posedge clock) begin
@@ -24,32 +23,15 @@ module perak_light (clock, reset, display, turn);
 
     always @ (diver) begin
         if (diver < 50000000) begin
-            clock_10hz = 1;
+            clock_1hz = 1;
         end else begin
-            clock_10hz = 0;
-        end
-    end
-
-    always@(posedge clock) begin
-        if(reset || diver250 == 400000) begin
-            diver250 = 0;
-        end else begin
-            diver250 = diver250 + 1;
-        end
-    end
-
-    always @ (diver250) begin
-        if (diver250 < 200000) begin
-            clock_250hz = 1;
-        end else begin
-            clock_250hz = 0;
+            clock_1hz = 0;
         end
     end
 
     // 此用於模擬檢測使用，實際燒錄時將此註解
     // always @ (clock) begin
-    //    clock_10hz = clock;
-    //    clock_250hz = clock;
+    //    clock_1hz = clock;
     // end
 
     always @ (data) begin
@@ -70,16 +52,26 @@ module perak_light (clock, reset, display, turn);
         endcase
     end
 
-    always @ (posedge clock_10hz) begin
-        if (reset || turn == 8) begin
-            turn = 4'b0001;
-        end else begin
-            turn = turn * 2;
-        end
+    always @ (data) begin
+        case (data)
+            0: turn = 4'b0001;
+            1: turn = 4'b0001;
+            2: turn = 4'b0001;
+            3: turn = 4'b0001;
+            4: turn = 4'b0010;
+            5: turn = 4'b0100;
+            6: turn = 4'b1000;
+            7: turn = 4'b1000;
+            8: turn = 4'b1000;
+            9: turn = 4'b1000;
+            10: turn = 4'b0100;
+            11: turn = 4'b0010;
+            default: turn = 4'b0001;
+        endcase
 	end
 
-    always @ (posedge clock_10hz) begin
-        if (reset || data == 12) begin
+    always @ (posedge clock_1hz) begin
+        if (reset || data == 11) begin
             data = 0;
         end else begin
             data = data + 1;
