58a1437de07ea428233a2b595cfc66dee08e9fc7
fetnet4101 <fetnet4101@gmail.com>
2022-11-15 21:08:07 +0800

    11/15

---
+++ example/verilog/WORK/full_adder/full_adder_assing_test.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module full_adder_assign_test ();
+
+reg a,b,ci;
+
+wire s, co;
+
+full_adder_assign UUT (a, b, ci, s, co);
+
+initial begin
+  {a, b, ci} = 3'b000;
+  #100; {a, b, ci} = 3'b001;
+  #100; {a, b, ci} = 3'b010;
+  #100; {a, b, ci} = 3'b011;
+  #100; {a, b, ci} = 3'b100;
+  #100; {a, b, ci} = 3'b101;
+  #100; {a, b, ci} = 3'b110;
+  #100; {a, b, ci} = 3'b111;
+
+end
+
+initial begin
+  #900;
+  $stop;
+end
+
+endmodule // full_add_one_test
---
+++ example/verilog/WORK/full_adder/full_adder_if.v
@@ -0,0 +1,31 @@
+module full_adder_if(a, b, ci, s, co);
+    input a, b, ci;
+    output s, co;
+
+    always@(a or b or ci)
+    if(a == 0 && b == 0 && ci == 0)begin
+        s = 0; co = 0;
+    end
+    else if(a == 0 && b == 0 && ci == 1)begin
+        s = 1; co = 0;
+    end
+    else if(a == 0 && b == 1 && ci == 0)begin
+        s = 1; co = 0;
+    end
+    else if(a == 0 && b == 1 && ci == 1)begin
+        s = 0; co = 1;
+    end
+    else if(a == 1 && b == 0 && ci == 0)begin
+        s = 1; co = 0;
+    end
+    else if(a == 1 && b == 0 && ci == 1)begin
+        s = 0; co = 1;
+    end
+    else if(a == 1 && b == 1 && ci == 0)begin
+        s = 0; co = 1;
+    end
+    else if(a == 1 && b == 1 && ci == 1)begin
+        s = 1; co = 1;
+    end
+
+endmodule
