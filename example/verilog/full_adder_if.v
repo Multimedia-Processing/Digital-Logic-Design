3468689d3b34ebe5804f13181bca28586491d52a
L.HT <l.hsuanting@gmail.com>
2022-11-15 17:53:47 +0800

    Change full_adder_if.v can pass

--- example/verilog/full_adder_if.v
+++ example/verilog/full_adder_if.v
@@ -2,37 +2,38 @@ module full_adder_if(a, b, ci, s, co);
 
 input a, b, ci;
 output s, co;
+reg s, co;
 
 always@(a or b or ci)
-if(a == 0 && b == 0 && ci = 0)
+if(a == 0 && b == 0 && ci == 0)
 begin
     s = 0; co = 0;
 end
-else if(a == 0 && b == 0 && ci = 1)
+else if(a == 0 && b == 0 && ci == 1)
 begin
     s = 1; co = 0;
 end
-else if(a == 0 && b == 1 && ci = 0)
+else if(a == 0 && b == 1 && ci == 0)
 begin
     s = 1; co = 0;
 end
-else if(a == 0 && b == 1 && ci = 1)
+else if(a == 0 && b == 1 && ci == 1)
 begin
     s = 0; co = 1;
 end
-else if(a == 1 && b == 0 && ci = 0)
+else if(a == 1 && b == 0 && ci == 0)
 begin
     s = 1; co = 0;
 end
-else if(a == 1 && b == 0 && ci = 1)
+else if(a == 1 && b == 0 && ci == 1)
 begin
     s = 0; co = 1;
 end
-else if(a == 1 && b == 1 && ci = 0)
+else if(a == 1 && b == 1 && ci == 0)
 begin
     s = 0; co = 1;
 end
-else if(a == 1 && b == 1 && ci = 1)
+else if(a == 1 && b == 1 && ci == 1)
 begin
     s = 1; co = 1;
 end
