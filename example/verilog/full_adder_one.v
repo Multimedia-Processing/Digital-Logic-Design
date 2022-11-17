1b2a989f9a3d50dc23a86bd4417098d4664116a7
linc7145 <linc7145@gmail.com>
2022-11-17 21:49:32 +0800

    刪掉不需要的

--- example/verilog/full_adder_one.v
+++
@@ -1,8 +0,0 @@
-module full_adder_one (a, b, ci, out);
-
-
-input  a, b, ci;
-output [1:0]out;
-assign out= a+ b+ ci;
-
-endmodule // full_add_one
--- example/verilog/nor_gate.v
+++
@@ -1,7 +0,0 @@
-module nor_gate(a,b,nor_output);
-
-input a,b;
-output nor_output;
-nor(nor_output,a,b);
-
-endmodule
