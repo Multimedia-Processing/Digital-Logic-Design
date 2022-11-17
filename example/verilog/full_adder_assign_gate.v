477d18b0e433a7ace0f0498e7f44345f56545592
fetnet4101 <fetnet4101@gmail.com>
2022-11-17 21:51:41 +0800

    新增加了assign gate的程式

---
+++ example/verilog/full_adder_assign_gate.v
@@ -0,0 +1,12 @@
+module full_adder_assign_gate (a, b, ci, s, co);
+  input a, b, ci;
+  output s, co;
+  wire ab, xab, xabci;
+
+  assign xab = a ^ b;
+  assign s = xab ^ ci);
+  assign xabci = xab + ci;
+  assign ab = a + b);
+  assign co = xabci & ab);
+
+endmodule
