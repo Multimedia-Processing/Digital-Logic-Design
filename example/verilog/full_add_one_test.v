36b6ea4b85c3980a4369d266b7f1335fae455f6f
洪偉哲 <whung6143@gmail.com>
2022-11-17 21:53:11 +0800

    移動檔案位置

--- example/verilog/full_add_one_test.v
+++ example/verilog/full_add_one_test.v
@@ -1,15 +1,14 @@
 `timescale 1ns / 1ps
-`include "full_adder_one.v"
 
 module full_adder_one_test ();
 
-reg a = 1'b0;
-reg b = 1'b0;
-reg ci = 1'b0;
+reg a; 
+reg b;
+reg ci;
 
 wire co, s;
 
-full_add_one UUT (a, b, ci, co, s);
+full_adder_one UUT (a, b, ci, co, s);
 
 initial begin
   #100; {a, b, ci} = 3'b001;
--- example/verilog/full_adder_case.v
+++ example/verilog/full_adder_case.v
@@ -1,41 +1,20 @@
-module full_adder_case(a, b, ci, s, co);
+module full_adder_case (A, B, Ci, Co, S);
+input A, B, Ci;
+output Co, S;
+reg Co, S;
 
-input a, b, ci;
-output s, co;
-reg s,co;
 
-always@ (a or b or ci)
-begin
-    case({a, b, ci})
-    3'b000 :begin 
-        s = 0; co = 0; 
-    end
-    3'b001 :begin 
-        s = 1; co = 0; 
-    end
-    3'b010 :begin 
-        s = 1; co = 0; 
-    end
-    3'b011 :begin 
-        s = 0; co = 1; 
-    end
-    3'b100 :begin 
-        s = 1; co = 0; 
-    end
-    3'b101 :begin 
-        s = 0; co = 1; 
-    end
-    3'b110 :begin 
-        s = 0; co = 1; 
-    end
-    3'b111 :begin 
-        s = 1; co = 1; 
-    end
-    endcase
+always @ (Ci or A or B) begin
+  case ({A, B, Ci}) //括號視為一個 //case設計增值表
+    0 : {Co, S} = 2'b00;
+    1 : {Co, S} = 2'b01;
+    2 : {Co, S} = 2'b01;
+    3 : {Co, S} = 2'b10;
+    4 : {Co, S} = 2'b01;
+    5 : {Co, S} = 2'b10;
+    6 : {Co, S} = 2'b10;
+    default: {Co, S} = 2'b11;
+  endcase
+
 end
-
-
-
-
-
-endmodule
+endmodule // full_adder_case
