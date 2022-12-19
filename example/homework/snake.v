9bf148acba1fb31765134c486314bf0c0fc8b987
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-12-19 20:45:27 +0800

    Relate #61 !53 提交將更新縮排
    修改項目:
    
    模組:

--- example/homework/snake.v
+++ example/homework/snake.v
@@ -195,11 +195,11 @@ module snake (clock, reset, turn, display);
             direction = 3;
         end else begin
             case (switch)
-              2'b1000: direction = 0;
-              2'b0100: direction = 1;
-              2'b1010: direction = 2;
-              2'b1001: direction = 3;
-              default: 0;
+                2'b1000: direction = 0;
+                2'b0100: direction = 1;
+                2'b1010: direction = 2;
+                2'b1001: direction = 3;
+                default: 0;
             endcase
         end
     end
