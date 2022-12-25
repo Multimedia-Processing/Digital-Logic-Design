399acc7bf1829057782bab0460c2ac4c81c952cd
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-12-25 15:14:09 +0800

    Relate #61 !53 提交將加入測試檔案與修改成可以檢視
    修改項目:
    
    模組:

--- example/homework/snake_test.v
+++ example/homework/snake_test.v
@@ -1,58 +1,52 @@
-`include "arithmetic_microoperation.v"
+`include "snake.v"
 
-module arithmetic_microoperation_test ();
+module snake_test ();
     reg clock, reset;
     reg [3:0] switch;
-    wire [7:0] trun;
+    wire [7:0] turn;
     wire [7:0] display;
+    wire [17:0] first, second, third, fourth;
 
     integer number, number2;
     integer seed;
 
-    arithmetic_microoperation UUT (clock, reset, switch, turn, display);
+    snake UUT (clock, reset, switch, first, second, third, fourth, turn, display);
 
     initial begin
         seed = 10;
     end
+
     initial begin
-        $display("| clock | reset | switch | turn | display |");
-        clock = 1'b1;
-        reset = 1'b1;
+        $display("| clock | reset | switch |        first        | second | third | fourth | turn | display |");
+        clock = 1;
+        reset = 0;
 
         #10;
+        reset = 1;
         #5;
-
-        reset = 1'b0;
-
-        #10;
-
-        for (number = 0; number < 8; number = number + 1) begin
-            for (number2 = 0; number2 < 32; number2 = number2 + 16) begin
-                {s, carry} = number;
-                a = $random(seed) % 16;
-                b = $random(seed) % 16;
-                #20;
-            end
+        for (number = 0; number < 18; number = number + 1) begin
+            switch = 0000;
+            #20;
         end
-        #10;
         reset = 1'b1;
+
+        for (number = 0; number < 4; number = number + 1) begin
+            switch = $random(seed) / 16;
+            #20;
+        end
 
         #20;
         $finish;
     end
 
     always begin
-        #10;
+        #5;
+        $monitor(
+            "|   %b   |   %b   |  %b  | %b | %b | %b | %b | %b | %b |",
+            clock, reset, switch, first, second, third, fourth, turn, display);
+
+        #5;
         clock = ~clock;
     end
 
-    always begin
-    #10;
-
-    $monitor(
-        "|   %b   |   %b   |   %b%b   | %d | %d |  %d |",
-        clock, reset, switch, turn, display);
-
-    end
-
-endmodule // arithmetic_microoperation_test
+endmodule // snake_test
