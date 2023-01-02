af83992b32be18c3b5fb5effb7f12dd9d54bc709
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-01-02 14:25:47 +0800

    Relate #61 !53 提交將修正暫存器
    修改項目:
    
    模組:

--- example/homework/lfsr_43.v
+++ example/homework/lfsr_43.v
@@ -3,8 +3,30 @@ module lfsr_43 (clock, reset, random);
     output [3:0] random;
 
     reg [3:0] random;
+    reg [27:0] diver;
+    reg clock_1hz;
 
-    always @ (posedge clock) begin
+    always@(posedge clock) begin
+        if(~reset || diver == 100000000) begin
+            diver = 0;
+        end else begin
+            diver = diver + 1;
+        end
+    end
+
+    always @ (diver) begin
+        if (diver < 50000000) begin
+            clock_1hz = 1;
+        end else begin
+            clock_1hz = 0;
+        end
+    end
+
+    // always @ (clock) begin
+    //     clock_1hz = clock;
+    // end
+
+    always @ (posedge clock_1hz) begin
         if (~reset) begin
             random = 4'b1000;
         end else begin
