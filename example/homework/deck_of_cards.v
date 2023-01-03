617f0c66f4a600941e35513d1f06e2e156572db1
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-01-04 00:48:45 +0800

    Relate #75 !51 提交將修改成可以顯示虛擬亂數產生撲克牌1~13數字
    修改項目:
    
    模組:

--- example/homework/deck_of_cards.v
+++ example/homework/deck_of_cards.v
@@ -5,14 +5,13 @@ module deck_of_cards (
             clock, reset, control, turn, display, random
         );
     input clock, reset, control;
-    output [7:0] turn;
+    output [3:0] turn;
     output [7:0] display;
     output [6:0] random;
 
-    reg [7:0] display;
     reg [32:0] diver;
     reg clock_400hz;
-    reg [7:0] turn;
+    reg [3:0] turn;
     reg [6:0] random;
     reg [3:0] select_bcd;
 
@@ -41,33 +40,29 @@ module deck_of_cards (
 
     always @ (posedge clock_400hz) begin
         if (~reset) begin
-            display = 8'b00000000;
-            random = 7'b1000000;
-            suits = 3'b100;
-            turn = 8'b00000001;
-        end else if (control) begin
-            random = {random[5:0], random[0] ^ random[2] ^ random[4] ^ random[6]};
+            random = 7'b0001000;
+            turn = 4'b0001;
+        end else if (control || random > 13) begin
+            random = {random[2:0], random[0] ^ random[3]};
+        end else if (turn >= 4'b0010) begin
+            turn = 4'b0001;
         end else begin
-            if (random < 14) begin
-                case (turn)
-                    8'b00000001: select_bcd = bcd[3:0];
-                    8'b00000010: select_bcd = bcd[7:4];
-                    8'b00000100: select_bcd = bcd[3:0];
-                    8'b00001000: select_bcd = bcd[7:4];
-                    8'b00010000: select_bcd = bcd[3:0];
-                    8'b00100000: select_bcd = bcd[7:4];
-                    8'b01000000: select_bcd = bcd[3:0];
-                    8'b10000000: select_bcd = bcd[7:4];
-                endcase
-            end
-
             turn = turn << 1;
         end
 
+    end
+    always @ (*) begin
+        case (turn)
+            4'b0001: select_bcd = bcd[3:0];
+            4'b0010: select_bcd = bcd[7:4];
+            4'b0100: select_bcd = bcd[3:0];
+            4'b1000: select_bcd = bcd[7:4];
+        endcase
     end
 
     binary_to_bcd row_bcd_circuit(random, bcd);
 
     seven_segment_display_assign seven_seg(select_bcd, display[7:1]);
+    assign display[0] = 0;
 
 endmodule // deck_of_cards
